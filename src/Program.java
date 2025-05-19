0a7588bb221884cccb1404ce31649289b3f18354
Arutogerudo <lara.altgeld22@gmail.com>
2025-05-19 08:23:55 +0200

    Startpunkt des Programms: Startet Simulation und ruft in parallelem Thread die durchgängige Aktualisierung der GUI und Backend-Zustände auf

---
+++ src/Program.java
@@ -0,0 +1,28 @@
+import HardwareAbstraction.ChargingState;
+import HardwareAbstraction.VoltageSimulator;
+
+public class Program {
+    public static void main(String[] args) {
+        VoltageSimulator simulator = new VoltageSimulator();
+        SimpleGUI gui = new SimpleGUI(simulator);
+        simulator.setState(ChargingState.DISCHARGING_ACTIVE);
+
+        new Thread(() -> {
+            while (true) {
+                simulator.tick();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            while (true) {
+                gui.update();
+            }
+        }).start();
+        
+    }
+}
