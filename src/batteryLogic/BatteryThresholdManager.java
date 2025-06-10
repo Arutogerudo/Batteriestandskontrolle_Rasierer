cc50ea0a5590e97e89fcfc0b9ccbf7b5c5271ebb
Arutogerudo <lara.altgeld22@gmail.com>
2025-06-10 23:39:28 +0200

    Sprint 3 initiales Setzen des LowBatteryThresholds vollständig mit file verknüpft

--- src/batteryLogic/BatteryThresholdManager.java
+++ src/batteryLogic/BatteryThresholdManager.java
@@ -7,24 +7,21 @@ class BatteryThresholdManager implements BatteryLogicConstants {
 
     private final BatteryStateCalculator calculator;
     private final VoltageSensor sensor;
-    private int lowBatteryThreshold;
 
-    BatteryThresholdManager(BatteryStateCalculator calculator, VoltageSensor sensor){
+    BatteryThresholdManager(BatteryStateCalculator calculator, VoltageSensor sensor) {
         this.calculator = calculator;
         this.sensor = sensor;
     }
 
-    boolean isLowBattery(CalibrationData calib){
-        return calculator.calculateStateOfCharge(sensor.readVoltage(), calib) <= lowBatteryThreshold;
+    boolean isLowBattery(CalibrationData calib) {
+        return calculator.calculateStateOfCharge(sensor.readVoltage(), calib) <= SettingsStorage.getInstance().readLowBatteryThresholdFromDisc();
     }
 
-    void updateLowBatteryThreshold(int newThreshold){
-        lowBatteryThreshold = newThreshold;
-        SettingsStorage storage = SettingsStorage.getInstance();
-        storage.setLowBatteryThreshold(newThreshold);
+    void updateLowBatteryThreshold(int newThreshold) {
+        SettingsStorage.getInstance().setLowBatteryThreshold(newThreshold);
     }
 
-    boolean isUndervoltageDetected(){
+    boolean isUndervoltageDetected() {
         return sensor.readVoltage() < UNDERVOLTAGE_LIMIT;
     }
 }
